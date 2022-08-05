5c24c8f77728e60043eaaeee9dd67f8017488d55
LeonardoCechellaVelho <leocechellav@gmail.com>
2022-08-04 21:12:35 -0300

    Change demo to use options

--- projects/image-writer-demo/src/app/app.component.ts
+++ projects/image-writer-demo/src/app/app.component.ts
@@ -7,25 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'image-writer-demo';
+  options = {
+    backgroundImage: 'https://png.pngtree.com/thumb_back/fw800/back_pic/04/49/70/73585bc614a0327.jpg',
+    imageHeight: 720,
+    imageWidth: 1080,
+    fields: [
+      {
+        textAlign: 'center',
+        content: 'LOREM IPSUM',
+        font: '40px Brush Script MT',
+        x: 540,
+        y: 150,
+        lineHeight: 30
+      },{
+        textAlign: 'center',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        font: '30px Brush Script MT',
+        x: 540,
+        y: 230,
+        lineHeight: 30,
+        lineWidth: 900
+      },{
+        textAlign: 'center',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        font: '30px Brush Script MT',
+        x: 540,
+        y: 500,
+        lineHeight: 30,
+        lineWidth: 900
+      }
+    ]
+  }
 
-  backgroundImage = 'https://instrucoes.atualcard.com.br/wp-content/uploads/2016/03/Marca-dágua-simples-l-Gráfica-Atual-Card.png';
-  imageHeight = 350;
-  imageWidth = 500;
-  fields = [
-    {
-      textAlign: 'center',
-      content: 'CERTIFICADO',
-      font: '30px Arial',
-      x: 250,
-      y: 50,
-      lineSize: 30
-    },{
-      textAlign: 'center',
-      content: 'Envolva o corpo da função em parênteses para retornar uma expressão literal de objeto',
-      font: '20px Arial',
-      x: 250,
-      y: 180,
-      lineSize: 30
-    }
-  ]
+  
 }
